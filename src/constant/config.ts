fa99c81c2042e1b2d4f9a9d2d74f175f347b3835
megicorapi <megicorapi@gmail.com>
2025-07-21 16:53:10 +0200

    update site title and description for Pokémon platform

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,5 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Pokémon',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+    "Catch'em all and build your dream squad. Explore every Pokémon and create your perfect team in one powerful and easy to use platform",
 };
