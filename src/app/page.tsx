5485e937025d7244ccfdd3283c52ba365d60740f
megicorapi <megicorapi@gmail.com>
2025-07-21 16:23:40 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function HomePage() {
+  return (
+    <main>
+          <footer className='absolute bottom-2 text-gray-700'>
+            © {new Date().getFullYear()} By{' '}Megi Corapi
+          </footer>
+    </main>
+  );
+}
