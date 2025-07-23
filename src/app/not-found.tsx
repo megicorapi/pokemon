e28f62b5d1b978ab239dec0e05ddeddff4f6dba9
megicorapi <megicorapi@gmail.com>
2025-07-23 11:25:10 +0200

    add new image for not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
 import * as React from 'react';
-import { RiAlarmWarningFill } from 'react-icons/ri';
 
 export const metadata: Metadata = {
   title: 'Not Found',
@@ -11,9 +11,12 @@ export default function NotFound() {
     <main>
       <section className='bg-white'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <RiAlarmWarningFill
-            size={60}
-            className='drop-shadow-glow animate-flicker text-red-500'
+          <Image
+            src='/images/broken-pokeball.png'
+            alt='Broken Pokeball'
+            width={224}  
+            height={224} 
+            className='drop-shadow-glow animate-flicker text-red-500 md:w-56'
           />
           <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
           <a href='/'>Back to home</a>
